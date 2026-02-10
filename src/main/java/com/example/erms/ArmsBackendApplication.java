dcbefb380f38045eb16e2aecc0ae7e01c167abde
Daivik Chauhan <daivikchauhan@Daiviks-MacBook-Air.local>
2026-02-10 16:31:19 +0530

    spring boot project is created

---
+++ src/main/java/com/example/erms/ArmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.erms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmsBackendApplication.class, args);
+	}
+
+}
